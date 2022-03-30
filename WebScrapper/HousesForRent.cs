408527c5e624fa4d7bafbb4b63706a868952ca17
alejob54 <alejob54@gmail.com>
2022-03-30 12:33:36 -0500

    Add project files.

---
+++ WebScrapper/HousesForRent.cs
@@ -0,0 +1,49 @@
+﻿using HtmlAgilityPack;
+using Telegram.Bot;
+
+namespace WebScrapper
+{
+    public class HousesForRent
+    {
+        public HousesForRent()
+        {
+
+        }
+
+        static string _SourceUrl = "https://vivepropiedadraiz.com.co/es/apartamento-en-arriendo/el-retiro";
+        static string _SourceHeader = "https://vivepropiedadraiz.com.co";
+        public static void LookForItems()
+        {
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument doc = web.Load(_SourceUrl);
+
+            var HeaderNames = doc.DocumentNode.SelectNodes("//div[@class='card-body-copy']");
+
+            foreach (var item in HeaderNames)
+            {
+                
+                var aa = item.InnerText;
+                var urlapto = item.SelectSingleNode("//div[@class='card-title']");
+                var cha = urlapto.ChildNodes["a"];
+                var aptoUrl = $"{_SourceHeader}{cha.GetAttributeValue("href", string.Empty)}";
+                var titlecontainer = item.SelectSingleNode("//a[@class='link-7']");
+
+                new TelegramBot().SendMessage("Encontré una nueva propiedad: \n" + aptoUrl);
+                return;
+            }
+        }
+    }
+
+    public class TelegramBot
+    {
+        private static string ApiKey = "TELEGRAM_API_KEY";
+
+        public void SendMessage(string msj)
+        {
+            string chatId = "yourChatId";
+
+            var bot = new TelegramBotClient(ApiKey);
+            var result = bot.SendTextMessageAsync(new Telegram.Bot.Types.ChatId(chatId), msj).Result;
+        }
+    }
+}
